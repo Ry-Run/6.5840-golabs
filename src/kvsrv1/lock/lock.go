0070b4d9979fe98436a6322b71de0b5d5eed213f
run <emailofrun@qq.com>
2025-07-25 21:56:55 +0800

    lab2-task2

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,6 +1,7 @@
 package lock
 
 import (
+	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 )
 
@@ -11,6 +12,9 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	l        string
+	cid      string
+	tversion rpc.Tversion
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -21,13 +25,39 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
 	// You may add code here
+	lk.l = l
+	lk.cid = kvtest.RandValue(8)
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		// 键值服务器有一个标识位，多客户端就来竞争这个标识位 l
+		value, tversion, err := lk.ck.Get(lk.l)
+		if err == rpc.ErrNoKey {
+			// 标识位未初始化
+			lk.tversion = 0
+		} else if value == "" {
+			// 标识位被释放
+			lk.tversion = tversion
+		} else {
+			// 标识位被占用，等待
+			continue
+		}
+
+		// 尝试抢占标识位
+		putErr := lk.ck.Put(lk.l, lk.cid, lk.tversion)
+		if putErr == rpc.OK {
+			// 抢占成功
+			lk.tversion.Inc()
+			return
+		}
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	// 释放标识位
+	lk.ck.Put(lk.l, "", lk.tversion)
 }
